4961a88b0a9d12815903595807174360a79c3bcc
Sleeyax <yourd3veloper@gmail.com>
2024-09-20 22:44:08 +0200

    feat: improve and simplify classifier

--- classification.py
+++ classification.py
@@ -1,80 +1,52 @@
-import stanza
+import time
+
 import torch
 from transformers import pipeline
-import re
-
-nlp = stanza.Pipeline('en', processors='tokenize')
 
 GPU_if_available = 0 if torch.cuda.is_available() else -1
-classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli", device=GPU_if_available)
+classifier = pipeline(
+    "zero-shot-classification",
+    model="MoritzLaurer/deberta-v3-large-zeroshot-v2.0",
+    device=GPU_if_available
+)
 
 
 class ClassificationResult:
     def __init__(
             self,
-            matched_topics: list,
-            is_relevant_topic: bool,
-            is_exact_topic: bool,
-            is_topic_found_in_question: bool,
-            confidence_points: int,
-            confidence_score: float
+            matched_topics: list[tuple[str, float]],
+            score_threshold: float,
+            execution_time: float,
+
     ):
         self.matched_topics = matched_topics
-        self.is_relevant_topic = is_relevant_topic
-        self.is_exact_topic = is_exact_topic
-        self.is_topic_found_in_question = is_topic_found_in_question
-        self.confidence_points = confidence_points
-        self.confidence_score = confidence_score
+        self.score_threshold = score_threshold
+        self.execution_time = execution_time
 
     def __str__(self):
         return str(self.__dict__)
 
 
-def _split_into_sentences(text):
-    doc = nlp(text)
-    sentences = [sentence.text for sentence in doc.sentences]
-    return sentences
+def classify(title: str, description: str, topics: list[str]):
+    start = time.time()
+    result = classifier(
+        f"{title}\n{description}",
+        topics,
+        hypothesis_template="This is an inquiry about {}",
+        multi_label=True
+    )
+    end = time.time()
+    execution_time = end - start
 
-
-def _match_topics(title, description, topics):
-    post_content = f"Title: {title}\nDescription: {description}"
-    result = classifier(post_content, topics, multi_label=True)
-    confidence_threshold = 0.5
-    matched_topics = [label for label, score in zip(result['labels'], result['scores']) if score > confidence_threshold]
-    return matched_topics if matched_topics else []
-
-
-def classify(title: str, description: str, tags: list):
-    post_content = f"{title} {description}".lower()
-    matched_topics = _match_topics(title, description, tags)
-
-    confidence_points = 0
-
-    # Determine whether the topic matches the post context using zero-shot classification
-    is_relevant_topic = len(matched_topics) > 0
-    if is_relevant_topic:
-        confidence_points += 1
-
-    # Check if the topic is textually mentioned in the post
-    is_exact_topic = any(topic.lower() in post_content for topic in matched_topics)
-    if is_exact_topic:
-        confidence_points += 1
-
-    # Check if the topic is textually mentioned in the form of a question
-    sentences = [title] + _split_into_sentences(description)
-    is_topic_found_in_question = any(
-        re.search(r"(" + "|".join(matched_topics) + ").*\\?", sentence) for sentence in sentences) if len(
-        matched_topics) > 0 else False
-    if is_topic_found_in_question:
-        confidence_points += 1
-
-    confidence_score = round(confidence_points / 3, 2)
+    score_threshold = 0.5
+    matched_topics = [
+        (label, score) for label,
+        score in zip(result['labels'], result['scores'])
+        if score > score_threshold
+    ]
 
     return ClassificationResult(
-        matched_topics,
-        is_relevant_topic,
-        is_exact_topic,
-        is_topic_found_in_question,
-        confidence_points,
-        confidence_score
+        matched_topics if matched_topics else [],
+        score_threshold,
+        execution_time,
     )
--- main.py
+++ main.py
@@ -2,22 +2,19 @@ from concurrent import futures
 import logging
 import grpc
 from service_pb2_grpc import ClassifierServicer, add_ClassifierServicer_to_server
-from service_pb2 import ClassificationResponse, ClassificationRequest
+from service_pb2 import ClassificationResponse, ClassificationRequest, Topic
 from classification import classify
 
 
 class Classifier(ClassifierServicer):
     def Classify(self, request: ClassificationRequest, context: grpc.ServicerContext) -> ClassificationResponse:
-        tags = [tag for tag in request.tags]
-        result = classify(request.title, request.body, tags)
+        request_topics = [topic for topic in request.topics]
+        result = classify(request.title, request.body, request_topics)
+        response_topics = [Topic(label=label, score=score) for label, score in result.matched_topics]
         return ClassificationResponse(
-            is_lead=result.is_relevant_topic,
-            is_exact_topic=result.is_exact_topic,
-            is_topic_found_in_question=result.is_topic_found_in_question,
-            confidence_points=result.confidence_points,
-            confidence_score=result.confidence_score,
-            matched_topics=result.matched_topics,
-            is_relevant_topic=result.is_relevant_topic,
+            matched_topics=response_topics,
+            execution_time=result.execution_time,
+            score_threshold=result.score_threshold,
         )
 
 
